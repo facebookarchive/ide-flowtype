1e3faf6e5b7b1e3086c67dfaed62a33c8d898b58
Will Binns-Smith <wbinnssmith@fb.com>
2017-06-26 11:17:14 -0700

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @noflow
+ */
+'use strict';
+
+/* eslint comma-dangle: [1, always-multiline], prefer-object-spread/prefer-object-spread: 0 */
+
+const rulesDirPlugin = require('eslint-plugin-rulesdir');
+rulesDirPlugin.RULES_DIR = 'eslint-rules';
+
+module.exports = {
+  "extends": "fbjs-opensource",
+  "plugins": ["rulesdir"],
+  "rules": {
+    "rulesdir/license-header": 1,
+  }
+}
