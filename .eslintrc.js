854fdb4656e9d311708eb5cf4187996415e49fbe
Will Binns-Smith <wbinnssmith@fb.com>
2017-07-17 17:20:26 -0700

    Use the proper data dir and describe in settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @noflow
+ */
+'use strict';
+
+/* eslint comma-dangle: [1, always-multiline], prefer-object-spread/prefer-object-spread: 0 */
+
 const rulesDirPlugin = require('eslint-plugin-rulesdir');
 rulesDirPlugin.RULES_DIR = 'eslint-rules';
 
@@ -6,13 +20,5 @@ module.exports = {
   "plugins": ["rulesdir"],
   "rules": {
     "rulesdir/license-header": 1,
-    // unfortunately have to ignore in fn calls without babel or recent v8
-    "comma-dangle": [
-      "error", {
-        "arrays": "only-multiline",
-        "objects": "only-multiline",
-        "functions": "ignore",
-      },
-    ]
   }
 }
--- eslint-rules/license-header.js
+++ eslint-rules/license-header.js
@@ -1,17 +1,16 @@
 /**
- * Copyright (c) 2015-present, Facebook, Inc.
+ * Copyright (c) 2017-present, Facebook, Inc.
  * All rights reserved.
  *
- * This source code is licensed under the license found in the LICENSE file in
- * the root directory of this source tree.
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
  *
  * @noflow
  */
-
+'use strict';
 
 /* eslint comma-dangle: [1, always-multiline], prefer-object-spread/prefer-object-spread: 0 */
-
-const path = require('path');
 
 const FAKE_DISABLE_RE = /\s*eslint-disable\s+nuclide-internal\/license-header\s*/;
 
